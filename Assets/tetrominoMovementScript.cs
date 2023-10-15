a094c71326b8132ecd2763d1b05d54655c8b2c90
ayd0 <73216273+ayd0@users.noreply.github.com>
2023-10-15 13:36:35 -0700

    updated movement and spawn key parameters for tetrominos

--- Assets/tetrominoMovementScript.cs
+++ Assets/tetrominoMovementScript.cs
@@ -6,31 +6,38 @@ public class tetrominoMovementScript : MonoBehaviour
 {
     private GameObject logicObject;
     private tetrominoLogicScript logicScript;
+    private GameObject tSpawnerObject;
+    private tetrominoSpawnerScript spawnerScript;
 
     // Start is called before the first frame update
     void Start()
     {
         logicObject = GameObject.FindGameObjectWithTag("TetrominoLogicObject");
         logicScript = logicObject.GetComponent<tetrominoLogicScript>();
+        tSpawnerObject = GameObject.FindGameObjectWithTag("TetrominoSpawnerObject");
+        spawnerScript = tSpawnerObject.GetComponent<tetrominoSpawnerScript>();
     }
 
     // Update is called once per frame
     void Update()
     {
-       if (Input.GetKeyDown(KeyCode.DownArrow))
+        if (!Input.GetKey(spawnerScript.buildModifierKey))
         {
-            gameObject.transform.position += new Vector3(0, -logicScript.tetrominoBounds.y, 0);
-            logicScript.lastPos = gameObject.transform.position;
-        } 
-       if (Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            gameObject.transform.position += new Vector3(-logicScript.tetrominoBounds.x, 0, 0);
-            logicScript.lastPos = gameObject.transform.position;
-        } 
-       if (Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            gameObject.transform.position += new Vector3(logicScript.tetrominoBounds.x, 0, 0);
-            logicScript.lastPos = gameObject.transform.position;
-        } 
+            if (Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                gameObject.transform.position += new Vector3(0, -logicScript.tetrominoBounds.y, 0);
+                logicScript.lastPos = gameObject.transform.position;
+            }
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                gameObject.transform.position += new Vector3(-logicScript.tetrominoBounds.x, 0, 0);
+                logicScript.lastPos = gameObject.transform.position;
+            }
+            if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                gameObject.transform.position += new Vector3(logicScript.tetrominoBounds.x, 0, 0);
+                logicScript.lastPos = gameObject.transform.position;
+            }
+        }
     }
 }
--- Assets/tetrominoSpawnerScript.cs
+++ Assets/tetrominoSpawnerScript.cs
@@ -7,6 +7,8 @@ public class tetrominoSpawnerScript : MonoBehaviour
     private GameObject logicObject;
     private tetrominoLogicScript logicScript;
     public GameObject tetromino;
+    public KeyCode buildModifierKey = KeyCode.LeftShift;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,21 +19,50 @@ public class tetrominoSpawnerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Vector3 spawnCoords = logicScript.lastPos;
         if (Input.GetKeyDown(KeyCode.F2))
         {
-            Vector3 spawnCoords = logicScript.lastPos;
-            if (spawnCoords == new Vector3(0,0,0))
+            if (spawnCoords == new Vector3(0, 0, 0))
             {
                 // initial case
                 spawnCoords = new Vector3(.1F, .1F, .1F);
                 Renderer renderer = tetromino.GetComponent<Renderer>();
                 logicScript.tetrominoBounds = renderer.bounds.size;
-            } else
+            }
+            else
             {
                 spawnCoords += new Vector3(logicScript.tetrominoBounds.x, 0, 0);
             }
-            Instantiate(tetromino, spawnCoords, transform.rotation);
-            logicScript.lastPos = spawnCoords;
+            instantiateTetremino(spawnCoords);
         }
+        if (Input.GetKey(buildModifierKey))
+        {
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                spawnCoords += new Vector3(-logicScript.tetrominoBounds.x, 0, 0);
+                instantiateTetremino(spawnCoords);
+            }
+            if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                spawnCoords += new Vector3(logicScript.tetrominoBounds.x, 0, 0);
+                instantiateTetremino(spawnCoords);
+            }
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                spawnCoords += new Vector3(0, logicScript.tetrominoBounds.y, 0);
+                instantiateTetremino(spawnCoords);
+            }
+            if (Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                spawnCoords += new Vector3(0, -logicScript.tetrominoBounds.y, 0);
+                instantiateTetremino(spawnCoords);
+            }
+        }
+    }
+
+    void instantiateTetremino(Vector3 spawnCoords)
+    {
+        Instantiate(tetromino, spawnCoords, transform.rotation);
+        logicScript.lastPos = spawnCoords;
     }
 }
